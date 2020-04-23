a77bd0d0da70530633e6fcd34119ad9a69994fdb
Victor Aiyeola <aiyeolavictor@gmail.com>
2020-04-23 01:16:16 +0100

    Deploy Heroku

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 /* eslint-disable operator-linebreak */
 export const BASE_URL =
   process.env.NODE_ENV === 'production'
-    ? process.env.BASE_URL
+    ? 'https://aboyeji-barnes-backstars.herokuapp.com'
     : 'http://localhost:4000';
 
 const token = `Bearer ${localStorage.getItem('barnesToken')}`;
