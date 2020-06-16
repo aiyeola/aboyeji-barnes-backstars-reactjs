9b9939139b2fb3cabd1b1a09ccee1ffd6dee541a
Victor Aiyeola <aiyeolavictor@gmail.com>
2020-06-16 11:45:21 +0100

    ft(test): enzyme setup for tests

--- src/config/index.js
+++ src/config/index.js
@@ -1,10 +1,10 @@
 /* eslint-disable operator-linebreak */
+const token = `Bearer ${localStorage.getItem('barnesToken')}`;
+
 export const BASE_URL =
   process.env.NODE_ENV === 'production'
     ? 'https://aboyeji-barnes-backstars.herokuapp.com'
     : 'http://localhost:4000';
-
-const token = `Bearer ${localStorage.getItem('barnesToken')}`;
 
 export const config = {
   headers: {
