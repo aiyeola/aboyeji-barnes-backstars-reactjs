c0ec980399667eef0d0982137ca9efdf7126ea30
Victor Aiyeola <aiyeolavictor@gmail.com>
2020-08-03 16:20:39 +0100

    refactor test to work

--- src/__tests__/components/LoginPage.spec.js
+++ src/__tests__/components/LoginPage.spec.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import { shallow, mount } from 'enzyme';
-import LoginPage from '../../components/LoginPage';
+import ReactDOM from 'react-dom';
+// import { render } from '@testing-library/react';
+import { getQueriesForElement } from '@testing-library/dom';
+import { LoginPage } from '../../components/LoginPage';
+
+const render = (component) => {
+  const root = document.createElement('div');
+  ReactDOM.render(component, root);
+  return getQueriesForElement;
+};
 
 describe('Login Page', () => {
   it('Should render without crashing', () => {
-    const wrapper = shallow(<LoginPage />);
-    console.log('wrapper: ', wrapper);
+    const { getByText } = render(<LoginPage />);
+
+    getByText('Sign Up Now!');
   });
-  const wrapper = mount(<LoginPage />);
-  console.log('mount: ', wrapper);
 });
