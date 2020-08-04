f6a286b12d90420656b11c91f8acf6549e0f53d2
Victor Aiyeola <aiyeolavictor@gmail.com>
2020-08-04 08:21:39 +0100

    Change test

---
+++ src/redux/reducers/addSupplierReducer.js
@@ -0,0 +1,24 @@
+import {
+  ADD_SUPPLIER_SUCCESS,
+  ADD_SUPPLIER_ERROR
+} from '../actions/actionTypes';
+
+const addSupplierReducer = (state = { data: null, error: null }, action) => {
+  const { type, details, error } = action;
+  switch (type) {
+    case ADD_SUPPLIER_SUCCESS:
+      return {
+        ...state,
+        data: details
+      };
+    case ADD_SUPPLIER_ERROR:
+      return {
+        ...state,
+        error: error
+      };
+    default:
+      return state;
+  }
+};
+
+export default addSupplierReducer;
