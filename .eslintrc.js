2fceb227e30ced12caf0e22f5080cd849c7e062f
Victor Aiyeola <aiyeolavictor@gmail.com>
2021-05-13 12:26:51 +0100

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,31 +16,10 @@ module.exports = {
     'plugin:react/recommended', // Uses the recommended rules from @eslint-plugin-react
     'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
     'prettier',
+    'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
   rules: {
     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
     // e.g. "@typescript-eslint/explicit-function-return-type": "off",
   },
 };
-
-// {
-//   "extends": [
-//     "react-app",
-//     "airbnb",
-//     "plugin:jsx-a11y/recommended",
-//     "prettier",
-//     "prettier/react",
-//     "plugin:react/recommended",
-//     "plugin:react-hooks/recommended"
-//   ],
-//   "plugins": ["jsx-a11y", "prettier", "react", "import"],
-//   "rules": {
-//     "semi": 1,
-//     "comma-dangle": 0,
-//     "arrow-body-style": 1,
-//     "linebreak-style": [0, "windows"],
-//     "react/jsx-filename-extension": [0, { "extensions": ["*.js", "*.jsx"] }],
-//     "react/forbid-prop-types": 0,
-//     "react/prefer-stateless-function": 0
-//   }
-// }
