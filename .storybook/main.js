191df2e784e3bdf3de492f06aad5d6416a9e40ed
Victor Aiyeola <aiyeolavictor@gmail.com>
2021-05-13 12:25:46 +0100

    clear project

--- .storybook/main.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  stories: ['../src/**/*.stories.[tj]s'],
-  addons: [
-    '@storybook/addon-actions/register',
-    '@storybook/addon-knobs/register',
-    '@storybook/addon-notes/register'
-  ]
-};
